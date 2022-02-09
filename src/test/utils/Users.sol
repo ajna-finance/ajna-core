ce848d11c5a413e15038415a5c34cbec55dab0fd
Filip Malachowicz <filip@makerdao.com>
2022-02-09 11:53:51 +0100

    Back to 0.8.10

--- src/test/utils/Users.sol
+++ src/test/utils/Users.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.11;
+pragma solidity 0.8.10;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20PerpPool} from "../../ERC20PerpPool.sol";
