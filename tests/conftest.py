9fa685deed6350fbb21f3d7f8718c55b087eb2af
grandizzy <grandizzy.the.egg@gmail.com>
2022-02-11 12:00:05 +0200

    Add more tests, improve

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,7 +27,7 @@ def mkr_dai_pool(mkr, dai, deployer):
 
 @pytest.fixture
 def lenders(dai, mkr_dai_pool, accounts):
-    amount = 100_000 * 10**18  # 100000 DAI for each lender
+    amount = 200_000 * 10**18  # 100000 DAI for each lender
     reserve = accounts.at("0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643", force=True)
     lenders = []
     for index in range(10):
@@ -57,3 +57,42 @@ def borrowers(mkr, mkr_dai_pool, accounts):
         )
         borrowers.append(borrower)
     yield borrowers
+
+
+class TestUtils:
+    @staticmethod
+    def assert_lender_quote_deposit(lender, amount, price, dai, mkr_dai_pool):
+        balance = dai.balanceOf(lender)
+        assert balance > amount
+        mkr_dai_pool.depositQuoteToken(amount, price, {"from": lender})
+        assert balance - dai.balanceOf(lender) == amount
+        assert mkr_dai_pool.quoteBalances(lender) == amount
+
+    @staticmethod
+    def assert_borrower_collateral_deposit(borrower, amount, mkr, mkr_dai_pool):
+        balance = mkr.balanceOf(borrower)
+        assert balance > amount
+        mkr_dai_pool.depositCollateral(amount, {"from": borrower})
+        assert balance - mkr.balanceOf(borrower) == amount
+        assert mkr_dai_pool.collateralBalances(borrower) == amount
+
+    @staticmethod
+    def assert_borrow(borrower, amount, dai, mkr_dai_pool):
+        mkr_dai_pool.borrow(amount, {"from": borrower})
+        assert dai.balanceOf(borrower) == amount
+
+    @staticmethod
+    def assert_bucket(bucket, deposit, debt, debitors, mkr_dai_pool):
+        onDeposit, totalDebitors, bucketDebt, _ = mkr_dai_pool.bucketInfo(bucket)
+        assert onDeposit == deposit
+        assert totalDebitors == debitors
+        assert bucketDebt == debt
+
+    @staticmethod
+    def assert_borrower_debt(borrower, bucket, expected, mkr_dai_pool):
+        assert mkr_dai_pool.userDebt(borrower, bucket) == expected
+
+
+@pytest.fixture
+def test_utils():
+    return TestUtils
