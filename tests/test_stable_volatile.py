2757a9014b55c2abe4ec62df7b459ab47c63ff29
Ed Noepel <ed@ajna.finance>
2022-04-01 20:28:45 -0400

    measure gas for borrowing and adding more liquidity

--- tests/test_stable_volatile.py
+++ tests/test_stable_volatile.py
@@ -1,8 +1,10 @@
 import brownie
+import inspect
 import pytest
 import random
+from brownie import Contract
 from decimal import *
-from sdk import AjnaPoolClient
+from sdk import AjnaPoolClient, AjnaProtocol
 
 
 MIN_BUCKET = 1543  # 2210.03602
@@ -10,7 +12,7 @@ MAX_BUCKET = 1623  # 3293.70191
 
 
 @pytest.fixture
-def pool_client(ajna_protocol, weth, dai):
+def pool_client(ajna_protocol: AjnaProtocol, weth, dai) -> AjnaPoolClient:
     return ajna_protocol.get_pool(weth.address, dai.address)
 
 
@@ -30,7 +32,7 @@ def lenders(ajna_protocol, pool_client, weth_dai_pool):
 @pytest.fixture
 def borrowers(ajna_protocol, pool_client, weth_dai_pool):
     weth_client = pool_client.get_collateral_token()
-    amount = 67 * 1e18
+    amount = 134 * 1e18
     dai_client = pool_client.get_quote_token()
     borrowers = []
     for _ in range(100):
@@ -62,14 +64,14 @@ def add_initial_liquidity(lenders, pool_client, bucket_math):
 
 def place_random_bid(lender_index, pool_client, bucket_math):
     price_count = MAX_BUCKET - MIN_BUCKET
-    price_position = 1 - random.expovariate(lambd=7.5)
-
+    price_position = 1 - random.expovariate(lambd=5.0)
+    # print(f"price_position={price_position}, lambda=5.0")
     price_index = max(0, min(int(price_position * price_count), price_count)) + MIN_BUCKET
     price = bucket_math.indexToPrice(price_index)
     pool_client.deposit_quote_token(60_000 * 1e18, price, lender_index)
 
 
-def draw_initial_debt(borrowers, pool_client, target_utilization=0.6, limit_price=2210.03602 * 1e18):
+def draw_initial_debt(borrowers, pool_client, target_utilization=0.55, limit_price=2210.03602 * 1e18):
     # Borrowers 10-99 draw debt; borrowers 0-9 reserved for the actual test
     pool = pool_client.get_contract()
     weth = pool_client.get_collateral_token().get_contract()
@@ -78,20 +80,53 @@ def draw_initial_debt(borrowers, pool_client, target_utilization=0.6, limit_pric
         collateral_balance = weth.balanceOf(borrowers[borrower_index])
         pool_client.deposit_collateral(collateral_balance, borrower_index)
         borrow_amount = target_debt / 90
-        pool_client.borrow(borrow_amount, borrower_index, 0)
+        assert borrow_amount > 1e18
+        pool_client.borrow(borrow_amount, borrower_index, limit_price)
 
 
-def test_stable_volatile_one(pool1, dai, weth, lenders, borrowers, bucket_math, test_utils):
+def draw_debt(borrowers, pool, weth, limit_price=2210.03602 * 1e18):
+    # Borrowers 0-9 draw debt
+    for borrower_index in range(0, 10):
+        # Deposit all their collateral
+        collateral_balance = weth.balanceOf(borrowers[borrower_index])
+        assert collateral_balance > 1e18
+        pool.addCollateral(collateral_balance, {"from": borrowers[borrower_index]})
+        # Determine how much debt to draw
+        collateral_to_utilize = ((borrower_index % 3) + 2.5) / 6 * collateral_balance
+        # borrow_amount = int(round(collateral_to_utilize * pool.getPoolPrice() / 100)*100)
+        borrow_amount = collateral_to_utilize * pool.getPoolPrice()/1e18
+        print(f"borrower {borrower_index} drawing {borrow_amount/1e18} debt utilizing {collateral_to_utilize/1e18} collateral")
+        assert borrow_amount > 1e18
+        pool.borrow(borrow_amount, limit_price, {"from": borrowers[borrower_index]})
+
+
+def add_quote_token(lenders, pool, bucket_math):
+    # Lenders 0-10 add liquidity
+    for lender_index in range(0, 10):
+        hpb = pool.hdp()
+        hpb_index = bucket_math.priceToIndex(hpb)
+        print(f"hpb is {hpb/1e18} at index {hpb_index}")
+        deposit_index_offset = ((lender_index % 6) - 2) * 2
+        deposit_price = bucket_math.indexToPrice(hpb_index + deposit_index_offset)
+        print(f"lender {lender_index} adding liquidity at {deposit_price/1e18}")
+        pool.addQuoteToken(200_000 * 1e18, deposit_price, {"from": lenders[lender_index]})
+
+
+def test_stable_volatile_one(pool1, dai, weth, lenders, borrowers, bucket_math, test_utils, capsys):
     assert pool1.collateral() == weth
     assert pool1.quoteToken() == dai
-
     assert len(lenders) == 100
     assert len(borrowers) == 100
     assert weth.balanceOf(borrowers[0]) >= 67 * 1e18
-
-    print(test_utils.dump_book(pool1, bucket_math, MIN_BUCKET, MAX_BUCKET))
+    print("before:\n" + test_utils.dump_book(pool1, bucket_math, MIN_BUCKET, MAX_BUCKET))
+    print(f"total quote token: {pool1.totalQuoteToken()/1e18}")
     print(f"actual utilization: {pool1.getPoolActualUtilization()/1e18}")
     assert pool1.totalQuoteToken() > 2_700_000 * 1e18  # 50% utilization
     assert pool1.getPoolActualUtilization() > 0.50 * 1e18
 
+    with test_utils.GasWatcher(['borrow', 'addQuoteToken']):
+        draw_debt(borrowers, pool1, weth)
+        add_quote_token(lenders, pool1, bucket_math)
+
+    print("after:\n" + test_utils.dump_book(pool1, bucket_math, MIN_BUCKET, MAX_BUCKET))
     # assert False
