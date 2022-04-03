0612a95396459c7860950ba5c3f6514c65d31866
Ed Noepel <ed@lpl.io>
2022-04-03 16:06:38 -0400

    updateInterestRate

--- tests/test_stable_volatile.py
+++ tests/test_stable_volatile.py
@@ -87,44 +87,51 @@ def draw_initial_debt(borrowers, pool_client, target_utilization=0.55, limit_pri
         pool_client.borrow(borrow_amount, borrower_index, limit_price)
 
 
-def draw_debt(borrowers, pool, weth, chain, limit_price=2210.03602 * 1e18):
-    # Borrowers 0-9 draw debt
-    for borrower_index in range(0, 10):
-        # Deposit all their collateral
-        collateral_balance = weth.balanceOf(borrowers[borrower_index])
-        assert collateral_balance > 1e18
-        pool.addCollateral(collateral_balance, {"from": borrowers[borrower_index]})
-        # Determine how much debt to draw
-        collateral_to_utilize = ((borrower_index % 3) + 2.5) / 6 * collateral_balance
-        # borrow_amount = int(round(collateral_to_utilize * pool.getPoolPrice() / 100)*100)
-        borrow_amount = collateral_to_utilize * pool.getPoolPrice()/1e18
-        print(f"borrower {borrower_index} drawing {borrow_amount/1e18} debt utilizing {collateral_to_utilize/1e18} collateral")
-        assert borrow_amount > 1e18
-        pool.borrow(borrow_amount, limit_price, {"from": borrowers[borrower_index]})
-        chain.sleep((borrower_index + 1) * 300)
-
-
-def add_quote_token(lenders, pool, bucket_math, chain) -> dict:
-    # Lenders 0-10 add liquidity
+def draw_and_bid(lenders, borrowers, pool, weth, bucket_math, chain) -> dict:
     buckets_deposited = {}
-    for lender_index in range(0, 10):
-        hpb = pool.hdp()
-        hpb_index = bucket_math.priceToIndex(hpb)
-        index_offset = ((lender_index % 6) - 2) * 2
-        price = bucket_math.indexToPrice(hpb_index + index_offset)
-        lender = lenders[lender_index]
-        print(f"lender {lender_index} adding liquidity at {price / 1e18}")
-        pool.addQuoteToken(lender, 200_000 * 1e18, price, {"from": lender})
-        buckets_deposited[lender_index] = price
-        chain.sleep((lender_index + 1) * 300)
+    time_elapsed = 0
+    for user_index in range(0, 10):
+        draw_debt(borrowers[user_index], user_index, pool, weth)
+        interval = (user_index + 1) * 360  # 6 minutes for user 0, 60 minutes for user 9
+        chain.sleep(interval)
+        time_elapsed += interval
+        buckets_deposited[user_index] = add_quote_token(lenders[user_index], user_index, pool, bucket_math, chain)
+        chain.sleep(interval)
+        time_elapsed += interval
+        if time_elapsed > 269 * 13.4:
+            pool.updateInterestRate({"from": lenders[user_index]})
+            time_elapsed = 0
     return buckets_deposited
+
+
+def draw_debt(borrower, borrower_index, pool, weth, limit_price=2210.03602 * 1e18):
+    # Deposit all their collateral
+    collateral_balance = weth.balanceOf(borrower)
+    assert collateral_balance > 1e18
+    pool.addCollateral(collateral_balance, {"from": borrower})
+    # Determine how much debt to draw
+    collateral_to_utilize = ((borrower_index % 3) + 2.5) / 6 * collateral_balance
+    borrow_amount = collateral_to_utilize * pool.getPoolPrice()/1e18
+    print(f"borrower drawing {borrow_amount/1e18} debt utilizing {collateral_to_utilize/1e18} collateral")
+    assert borrow_amount > 1e18
+    pool.borrow(borrow_amount, limit_price, {"from": borrower})
+
+
+def add_quote_token(lender, lender_index, pool, bucket_math, chain):
+    # Lenders 0-10 add liquidity
+    hpb = pool.hdp()
+    hpb_index = bucket_math.priceToIndex(hpb)
+    index_offset = ((lender_index % 6) - 2) * 2
+    price = bucket_math.indexToPrice(hpb_index + index_offset)
+    print(f"lender adding liquidity at {price / 1e18}")
+    pool.addQuoteToken(lender, 200_000 * 1e18, price, {"from": lender})
+    return price
 
 
 def remove_quote_token(lenders, pool, buckets_deposited, chain):
     for lender_index, price in buckets_deposited.items():
         print(f"lender {lender_index} removing liquidity at {price / 1e18}")
         lender = lenders[lender_index]
-        # FIXME: getting ajna/amount-greater-than-claimable trying to withdraw full amount
         pool.removeQuoteToken(lender, 200_000 * 1e18, price, {"from": lender})
         chain.sleep((lender_index + 1) * 300)
 
@@ -141,9 +148,8 @@ def test_stable_volatile_one(pool1, dai, weth, lenders, borrowers, bucket_math, 
     assert pool1.totalQuoteToken() > 2_700_000 * 1e18  # 50% utilization
     assert pool1.getPoolActualUtilization() > 0.50 * 1e18
 
-    with test_utils.GasWatcher(['borrow', 'addQuoteToken', 'removeQuoteToken']):
-        draw_debt(borrowers, pool1, weth, chain)
-        buckets_deposited = add_quote_token(lenders, pool1, bucket_math, chain)
+    with test_utils.GasWatcher(['addQuoteToken', 'borrow', 'removeQuoteToken', 'updateInterestRate']):
+        buckets_deposited = draw_and_bid(lenders, borrowers, pool1, weth, bucket_math, chain)
         hpb_index = bucket_math.priceToIndex(pool1.hdp())
         print("After:\n" + test_utils.dump_book(pool1, bucket_math, MIN_BUCKET, hpb_index))
         remove_quote_token(lenders, pool1, buckets_deposited, chain)
