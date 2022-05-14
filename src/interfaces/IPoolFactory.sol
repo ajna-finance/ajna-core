428bcd798afe0050de75eb6951d6fed7ebcb0a34
lucas-manuel <lucas@maple.finance>
2022-05-14 12:53:50 -0400

    feat: add alignment in interfaces, update underscore syntax

--- src/interfaces/IPoolFactory.sol
+++ src/interfaces/IPoolFactory.sol
@@ -1,26 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.11;
+
 /**
  * @title Ajna Pool Factory
- * @dev Used to deploy both funigible and non fungible pools
-*/
+ * @dev   Used to deploy both funigible and non fungible pools
+ */
 interface IPoolFactory {
 
+    /*********************/
+    /*** Custom Errors ***/
+    /*********************/
+
     /**
-     * @notice Emitted when a new pool is created
-     * @param pool the address of the new pool
-    */
+     *  @notice Emitted when a new pool is created
+     *  @param pool the address of the new pool
+     */
     event PoolCreated(address pool);
 
-    /** @notice ETH cannot be used for collateral or quote token, use WETH */
+    /**
+     *  @notice ETH cannot be used for collateral or quote token, use WETH
+     */
     error WethOnly();
 
-    /** @notice a pool with same collateral and quote token already exists */
+    /**
+     *  @notice A pool with same collateral and quote token already exists
+     */
     error PoolAlreadyExists();
 
     /**
-     * @notice Deploys a cloned pool for the given collateral and quote token
-     * @dev Pool must not already exist, and must use WETH instead of ETH
-    */
+     *  @notice Deploys a cloned pool for the given collateral and quote token
+     *  @dev    Pool must not already exist, and must use WETH instead of ETH
+     *  @param  collateral_ Address of ERC20 collateral token
+     *  @param  collateral_ Address of ERC20 quote token
+     */
     function deployPool(address collateral_, address quote_) external returns (address pool_);
 }
