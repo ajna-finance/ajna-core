350c41a5c599597d6f7b276a1c9baf746dcc3aa7
Mike <mikehathaway@makerdao.com>
2022-06-07 17:23:14 -0400

    initial pr feedback

--- src/ERC20Pool.sol
+++ src/ERC20Pool.sol
@@ -4,8 +4,6 @@ pragma solidity 0.8.11;
 
 import { Clone } from "@clones/Clone.sol";
 
-import { console }     from "@std/console.sol";
-
 import { ERC20 }     from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
--- src/ERC721PoolFactory.sol
+++ src/ERC721PoolFactory.sol
@@ -23,25 +23,25 @@ contract ERC721PoolFactory is PoolDeployer {
         implementation = new ERC721Pool();
     }
 
-    function deployNFTCollectionPool(address collateral_, address quote_) external canDeploy(NON_SUBSET_HASH, collateral_, quote_) returns (address) {
+    function deployNFTCollectionPool(address collateral_, address quote_) external canDeploy(NON_SUBSET_HASH, collateral_, quote_) returns (address pool_) {
         bytes memory data = abi.encodePacked(collateral_, quote_);
 
         ERC721Pool pool = ERC721Pool(address(implementation).clone(data));
         pool.initialize();
+        pool_ = address(pool);
 
-        deployedPools[NON_SUBSET_HASH][collateral_][quote_] = address(pool);
-        emit PoolCreated(address(pool));
-        return address(pool);
+        deployedPools[NON_SUBSET_HASH][collateral_][quote_] = pool_;
+        emit PoolCreated(pool_);
     }
 
-    function deployNFTSubsetPool(address collateral_, address quote_, uint256[] memory tokenIds_) external canDeploy(getNFTSubsetHash(tokenIds_), collateral_, quote_) returns (address) {
+    function deployNFTSubsetPool(address collateral_, address quote_, uint256[] memory tokenIds_) external canDeploy(getNFTSubsetHash(tokenIds_), collateral_, quote_) returns (address pool_) {
         bytes memory data = abi.encodePacked(collateral_, quote_, tokenIds_);
 
         ERC721Pool pool = ERC721Pool(address(implementation).clone(data));
         pool.initializeSubset(tokenIds_);
+        pool_ = address(pool);
 
-        deployedPools[getNFTSubsetHash(tokenIds_)][collateral_][quote_] = address(pool);
-        emit PoolCreated(address(pool));
-        return address(pool);
+        deployedPools[getNFTSubsetHash(tokenIds_)][collateral_][quote_] = pool_;
+        emit PoolCreated(pool_);
     }
 }
