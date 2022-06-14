dbf7fd47584081604b478b741b9a6d009744f2cf
grandizzy <grandizzy.the.egg@gmail.com>
2022-06-14 09:00:28 +0300

    Remove hardhat from lib

--- src/ERC20Pool.sol
+++ src/ERC20Pool.sol
@@ -4,8 +4,6 @@ pragma solidity 0.8.14;
 
 import { Clone } from "@clones/Clone.sol";
 
-import { console } from "@hardhat/hardhat-core/console.sol"; // TESTING ONLY
-
 import { ERC20 }     from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
