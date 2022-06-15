dfab84944316eff3a5eb7444dfe2473b1113f493
grandizzy <grandizzy.the.egg@gmail.com>
2022-06-15 08:21:09 +0300

    Update pragma on NFT related code

--- src/_test/ERC721Pool/ERC721PoolFactory.t.sol
+++ src/_test/ERC721Pool/ERC721PoolFactory.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.11;
+pragma solidity 0.8.14;
 
 import { ERC721Pool }        from "../../ERC721Pool.sol";
 import { ERC721PoolFactory } from "../../ERC721PoolFactory.sol";
