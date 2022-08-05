be301a40a45bca1aa02e4970ba06ba6c6196d5aa
Mike <mikehathaway@makerdao.com>
2022-08-05 01:04:56 -0400

    fix ajna token test

--- src/tokens/Ajna.sol
+++ src/tokens/Ajna.sol
@@ -5,9 +5,9 @@ pragma solidity 0.8.14;
 import { ERC20 }       from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract AjnaToken is ERC20("AjnaToken", "AJNA"), ERC20Permit {
+contract AjnaToken is ERC20("Ajna", "AJNA"), ERC20Permit {
 
-    constructor(uint256 initialSupply) ERC20Permit("AjnaToken") {
+    constructor(uint256 initialSupply) ERC20Permit("Ajna") {
         _mint(msg.sender, initialSupply);
     }
 
