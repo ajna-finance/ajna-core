f5cd2e35ad0920059c314aefcedee7ab6a57c85b
grandizzy <grandizzy.the.egg@gmail.com>
2022-08-11 09:26:40 +0300

    Remove OZ's ERC721Enumerable from PositionNFT contract - fixes PositionManager contract size - not needed here (useful for things like NFT staking contracts to automatically pull up the list of tokenIds owned by someone. That info can be generated offchain)

--- src/base/PositionNFT.sol
+++ src/base/PositionNFT.sol
@@ -5,14 +5,13 @@ import { Base64 } from "@base64-sol/base64.sol";
 
 import { ERC721 }           from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { ERC721Burnable }   from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";  // TODO: determine if tokens should be burnable
-import { ERC721Enumerable } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import { Strings }          from "@openzeppelin/contracts/utils/Strings.sol";
 
 import { IPositionManager } from "./interfaces/IPositionManager.sol";
 
 import { PermitERC721 } from "./PermitERC721.sol";
 
-abstract contract PositionNFT is ERC721, ERC721Enumerable, PermitERC721 {
+abstract contract PositionNFT is ERC721, PermitERC721 {
 
     constructor(
         string memory name_, string memory symbol_, string memory version_
@@ -69,16 +68,6 @@ abstract contract PositionNFT is ERC721, ERC721Enumerable, PermitERC721 {
     // TODO: add SVG string for Ajna Logo
     function _renderTokenById(uint256) internal pure returns (string memory) {
         return string(abi.encodePacked(""));
-    }
-
-    /** @dev Override required by solidity to use ERC721Enumerable library */ 
-    function _beforeTokenTransfer(address from_, address to_, uint256 tokenId_) internal override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from_, to_, tokenId_);
-    }
-
-    /** @dev Override required by solidity to use ERC721Enumerable library */ 
-    function supportsInterface(bytes4 interfaceId_) public view override(ERC721, ERC721Enumerable) returns (bool) {
-        return super.supportsInterface(interfaceId_);
     }
 
 }
