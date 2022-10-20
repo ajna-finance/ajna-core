c7e1e9543fe7cefd82e6b92a3b1ee511c91c0606
Ed Noepel <ed@ajna.finance>
2022-10-19 21:43:54 -0400

    updated check-size script to show percentage

--- src/base/Pool.sol
+++ src/base/Pool.sol
@@ -3,7 +3,6 @@
 pragma solidity 0.8.14;
 
 import '@clones/Clone.sol';
-import "forge-std/console.sol";
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
