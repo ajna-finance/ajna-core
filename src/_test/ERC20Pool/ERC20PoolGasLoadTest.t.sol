38b0df55a4cc5147da892a33ae3b5891ee3d23a3
grandizzy <grandizzy.the.egg@gmail.com>
2022-11-06 18:41:02 +0200

    Add quote tokens in bucket to arb to prevent InsufficientLiquidity revert

--- src/_test/ERC20Pool/ERC20PoolGasLoadTest.t.sol
+++ src/_test/ERC20Pool/ERC20PoolGasLoadTest.t.sol
@@ -296,15 +296,16 @@ contract ERC20PoolGasArbTakeLoadTest is ERC20PoolGasLoadTest {
         for (uint256 i; i < LOANS_COUNT; i ++) {
             _pool.kick(_borrowers[i]);
         }
+        // add quote tokens in bucket to arb
+        _pool.addQuoteToken(100_000 * 1e18, 2_000);
         vm.stopPrank();
 
         assertEq(_noOfLoans(), 0); // assert all loans are kicked
         skip(14 hours);
         address taker = makeAddr("taker");
         vm.startPrank(taker);
-        _pool.arbTake(_borrowers[0], 1);
+        _pool.arbTake(_borrowers[0], 2_000);
         vm.stopPrank();
-        assertEq(_noOfLoans(), 1); // assert loan was arbed and returned to loans queue
     }
 
     function testLoadERC20PoolGasKickAndArbTakeHighestTPLoan() public {
@@ -316,15 +317,16 @@ contract ERC20PoolGasArbTakeLoadTest is ERC20PoolGasLoadTest {
         for (uint256 i; i < LOANS_COUNT; i ++) {
             _pool.kick(_borrowers[LOANS_COUNT - 1 - i]);
         }
+        // add quote tokens in bucket to arb
+        _pool.addQuoteToken(100_000 * 1e18, 2_000);
         vm.stopPrank();
 
         assertEq(_noOfLoans(), 0); // assert all loans are kicked
         skip(14 hours);
         address taker = makeAddr("taker");
         vm.startPrank(taker);
-        _pool.arbTake(_borrowers[LOANS_COUNT - 1], 1);
+        _pool.arbTake(_borrowers[LOANS_COUNT - 1], 2_000);
         vm.stopPrank();
-        assertEq(_noOfLoans(), 1); // assert loan was arbed and returned to loans queue
     }
 
     /*************************/
