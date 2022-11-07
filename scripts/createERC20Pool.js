23cdc74961f74ba0f0b6488789139a08287db174
grandizzy <grandizzy.the.egg@gmail.com>
2022-11-07 23:28:24 +0200

    Sample of creating factory pool using web3js
    - in a terminal run:
    brownie console
    run('sdk_setup')
    
    - in a different window (do not close brownie console), cd to script dir and
    npm install
    node createERC20Pool.js
    
    This will create a COMP/DAI ERC20 pool using properties within .env file generated in scripts dir. The private key from env file can be imported in Metamask for further tests

---
+++ scripts/createERC20Pool.js
@@ -0,0 +1,43 @@
+const Web3 = require("web3");
+
+// Loading the contract ABI
+// (the results of a previous compilation step)
+const fs = require("fs");
+const { abi } = JSON.parse(fs.readFileSync("../brownie_out/contracts/ERC20PoolFactory.json"));
+
+async function main() {
+  // Configuring the connection to an Ethereum node
+  const web3 = new Web3(
+    new Web3.providers.HttpProvider(
+      process.env.ETH_RPC_URL
+    )
+  );
+  // Creating a signing account from a private key
+  const signer = web3.eth.accounts.privateKeyToAccount(
+    process.env.PRIVATE_KEY
+  );
+  web3.eth.accounts.wallet.add(signer);
+  // Creating a Contract instance
+  const contract = new web3.eth.Contract(
+    abi,
+    // Replace this with the address of your deployed contract
+    process.env.ERC20_FACTORY
+  );
+  const interestRate = web3.utils.toWei(String(0.05), 'ether');
+  const tx = contract.methods.deployPool(process.env.COMP, process.env.DAI, interestRate);
+  // console.log(await tx.estimateGas())
+  const receipt = await tx
+    .send({
+      from: signer.address,
+      gas: 8_000_000,
+    })
+    .once("transactionHash", (txhash) => {
+      console.log(`Mining transaction ...`);
+    });
+  // The transaction is now on chain!
+  console.log(receipt);
+
+}
+
+require("dotenv").config();
+main();
