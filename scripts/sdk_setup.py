e6dc51035d20a0b99cffac948e34d6283336d062
grandizzy <38490174+grandizzy@users.noreply.github.com>
2022-11-11 11:38:59 +0200

    Sample of creating factory pool using web3js (#323)
    * Sample of creating factory pool using web3js
    
    - in a terminal run:
    brownie console
    run('sdk_setup')
    
    - in a different window (do not close brownie console), cd to script dir and
    npm install
    node createERC20Pool.js
    
    This will create a COMP/DAI ERC20 pool using properties within .env file generated in scripts dir. The private key from env file can be imported in Metamask for further tests
    
    * Estimate gas
    
    * add deployedPools call
    
    * add package lock
    
    Co-authored-by: xdemocle <xdemocle@gmail.com>

--- scripts/sdk_setup.py
+++ scripts/sdk_setup.py
@@ -9,13 +9,14 @@ def main():
     BucketMath.deploy({"from": accounts[0]})
     erc20_pool_factory = ERC20PoolFactory.deploy({"from": accounts[0]})
     erc721_pool_factory = ERC721PoolFactory.deploy({"from": accounts[0]})
-    dictionary = {
-        "erc20factory": erc20_pool_factory.address,
-        "erc721factory": erc721_pool_factory.address
-    }
-    
-    with open("brownie_out/ajna-sdk.json", "w") as outfile:
-        json.dump(dictionary, outfile, indent=4, sort_keys=True)
+
+    with open("scripts/.env", "w") as outfile:
+        outfile.write("ETH_RPC_URL=http://localhost:8545/")
+        outfile.write("\nPRIVATE_KEY=0xacd5fc4b1c3141f67b35f09210379295c34f7e5c33d6bf1755a65c3c07a9e854")
+        outfile.write("\nERC20_FACTORY="+erc20_pool_factory.address)
+        outfile.write("\nERC721_FACTORY="+erc721_pool_factory.address)
+        outfile.write("\nCOMP=0xc00e94Cb662C3520282E6f5717214004A7f26888")
+        outfile.write("\nDAI=0x6B175474E89094C44Da98b954EedeAC495271d0F")
 
     # read config and fund accounts
     with open('scripts/sdk-setup.json', 'r') as setupfile:
@@ -26,10 +27,10 @@ def main():
     dai_contract = Contract(dai_config.get('address'))
     dai_reserve = accounts.at(dai_config.get('reserve'), True);
 
-    mkr_config = sdk_config.get('tokens').get('MKR')
+    comp_config = sdk_config.get('tokens').get('COMP')
 
-    mkr_contract = Contract(mkr_config.get('address'))
-    mkr_reserve = accounts.at(mkr_config.get('reserve'), True);
+    comp_contract = Contract(comp_config.get('address'))
+    comp_reserve = accounts.at(comp_config.get('reserve'), True);
 
     bored_ape_config = sdk_config.get('tokens').get('BAYC')
     ape_contract = Contract(bored_ape_config.get('address'))
@@ -37,6 +38,7 @@ def main():
     test_accounts = sdk_config.get('accounts')
     for addresses in test_accounts.items():
         address = addresses[0]
+        accounts[0].transfer(address, 100 * 1e18)
 
         balances = test_accounts.get(address)
         for token in balances:
@@ -45,14 +47,22 @@ def main():
                 print(f"=== Transfer {balance} DAI to {address} ===")
                 dai_contract.transfer(address, balance * 1e18, {'from': dai_reserve})
 
-            if token == 'MKR':
+            if token == 'COMP':
                 balance = balances.get(token)
-                print(f"=== Transfer {balance} MKR to {address} ===")
-                mkr_contract.transfer(address, balance * 1e18, {'from': mkr_reserve})
+                print(f"=== Transfer {balance} COMP to {address} ===")
+                comp_contract.transfer(address, balance * 1e18, {'from': comp_reserve})
 
             if token == 'BAYC':
                 for tokenId in balances.get(token):         
                     print(f"=== Transfer Bored Ape {tokenId} to {address} ===")
                     ape_owner = accounts.at(ape_contract.ownerOf(tokenId), True);
                     ape_contract.transferFrom(ape_owner, address, tokenId, {'from': ape_owner})
+    
+    return (
+        erc20_pool_factory,
+        erc721_pool_factory,
+        comp_contract,
+        dai_contract,
+    )
+
 
