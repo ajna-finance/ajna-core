d48ba9c653614b05a34ac710291690eeed20e38d
Ed Noepel <ed@ajna.finance>
2022-11-22 19:55:30 -0500

    updated address

--- tests/forge/ERC20Pool/ERC20PoolFactory.t.sol
+++ tests/forge/ERC20Pool/ERC20PoolFactory.t.sol
@@ -8,7 +8,7 @@ import 'src/erc20/ERC20Pool.sol';
 import 'src/erc20/ERC20PoolFactory.sol';
 
 contract ERC20PoolFactoryTest is ERC20HelperContract {
-    address immutable poolAddress = 0x918ebA623C6291984Eeea4EE07335c3DDa03c0d3;
+    address immutable poolAddress = 0x9FE92fe72Ae1Bc5f008C3f405606717d43Fc468D;
 
     ERC20PoolFactory internal _poolFactory;
 
