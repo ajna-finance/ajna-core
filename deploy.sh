f4a5cc0c8cb72f63e8c50f652504da81229d34b4
Ed Noepel <ed@ajna.finance>
2022-12-06 16:34:39 -0500

    library linkage working

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# TODO
-# - store contract addresses in varibles
-# - pass addresses using --libraries to deploy factories (see https://book.getfoundry.sh/reference/forge/forge-create)
-
 read -p "Enter keystore password: " -s password
 
 regex="Deployed to: ([0-9xa-fA-F]+)"
-libraries=( Auctions LenderActions PoolCommons )
+linkage=()
 
+echo
 echo Deploying libraries...
+libraries=( Auctions LenderActions PoolCommons )
 for contract in "${libraries[@]}"
 do
     createlib="forge create --rpc-url ${ETH_RPC_URL:?} --keystore ${DEPLOY_KEY:?} --password ${password} \
@@ -17,13 +15,28 @@ do
     output=$($createlib)
     if [[ $output =~ $regex ]]
     then
-        echo Deployed $contract to ${BASH_REMATCH[1]}
+        address=${BASH_REMATCH[1]}
+        printf "Deployed %20s to %s\n" ${contract:0:20} $address
+        linkage+="--libraries src/libraries/external/$contract.sol:$contract:$address "
     else
         echo $contract was not deployed: $output
+        exit 1
     fi
 done
 
-# deploy factories
-# factories=( ERC20PoolFactory ERC721PoolFactory )
-forge create --rpc-url ${ETH_RPC_URL:?} --keystore ${DEPLOY_KEY:?} --password ${password} \
-    src/erc20/ERC20PoolFactory.sol:ERC20PoolFactory
+echo Deploying factories...
+factories=( ERC20PoolFactory ERC721PoolFactory )
+for contract in "${factories[@]}"
+do
+    createfactory="forge create --rpc-url ${ETH_RPC_URL:?} --keystore ${DEPLOY_KEY:?} --password ${password} \
+        src/erc20/ERC20PoolFactory.sol:ERC20PoolFactory ${linkage}"
+    output=$($createfactory)
+    if [[ $output =~ $regex ]]
+    then
+        address=${BASH_REMATCH[1]}
+        printf "Deployed %20s to %s\n" ${contract:0:20} $address
+    else
+        echo $contract was not deployed: $output
+        exit 2
+    fi
+done
