275960bea48526b261097d14bba4fff579926070
Ed Noepel <ed@ajna.finance>
2022-12-06 15:58:21 -0500

    interactively read password once

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # TODO
-# - interactively input password into variable, pass to each command
 # - store contract addresses in varibles
 # - pass addresses using --libraries to deploy factories (see https://book.getfoundry.sh/reference/forge/forge-create)
 
-regex="Deployed to: ([0-9xa-fA-F]+)"
-libraries=( Auctions BucketMath )
+read -p "Enter keystore password: " -s password
 
-# deploy libraries
+regex="Deployed to: ([0-9xa-fA-F]+)"
+libraries=( Auctions LenderActions PoolCommons )
+
+echo Deploying libraries...
 for contract in "${libraries[@]}"
 do
-    createlib="forge create --rpc-url ${ETH_RPC_URL:?} \
-	    --keystore ${DEPLOY_KEY:?} src/libraries/$contract.sol:$contract"    
+    createlib="forge create --rpc-url ${ETH_RPC_URL:?} --keystore ${DEPLOY_KEY:?} --password ${password} \
+        src/libraries/external/$contract.sol:$contract"    
     output=$($createlib)
     if [[ $output =~ $regex ]]
     then
@@ -22,8 +23,7 @@ do
     fi
 done
 
-
 # deploy factories
 # factories=( ERC20PoolFactory ERC721PoolFactory )
-# forge create --rpc-url ${ETH_RPC_URL:?} \
-#     --keystore ${DEPLOY_KEY:?} src/erc20/ERC20PoolFactory.sol:ERC20PoolFactory
+forge create --rpc-url ${ETH_RPC_URL:?} --keystore ${DEPLOY_KEY:?} --password ${password} \
+    src/erc20/ERC20PoolFactory.sol:ERC20PoolFactory
