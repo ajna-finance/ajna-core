40e3f84d57700363016f32499b2a7673f84c6b6f
grandizzy <grandizzy.the.egg@gmail.com>
2022-12-18 20:39:34 +0200

    Check NFT auction > MIN PRICE when settling NFT auction

--- src/libraries/external/Auctions.sol
+++ src/libraries/external/Auctions.sol
@@ -598,7 +598,7 @@ library Auctions {
                 auctions_.liquidations[borrowerAddress_].kickMomp,
                 auctions_.liquidations[borrowerAddress_].kickTime
             );
-            bucketIndex_ = auctionPrice != 0 ? _indexOf(auctionPrice) : MAX_FENWICK_INDEX;
+            bucketIndex_ = auctionPrice > MIN_PRICE ? _indexOf(auctionPrice) : MAX_FENWICK_INDEX;
             lps_ = Buckets.addCollateral(
                 buckets_[bucketIndex_],
                 borrowerAddress_,
