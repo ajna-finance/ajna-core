ff40be491a8bff3069848eef8420b8d35e40637f
Mike <mikehathaway@makerdao.com>
2022-12-21 13:52:59 -0500

    cleanup safeerc20namer

--- src/libraries/SafeERC20Namer.sol
+++ src/libraries/SafeERC20Namer.sol
@@ -2,56 +2,39 @@
 
 pragma solidity 0.8.14;
 
-import './AddressStringUtil.sol';
-
 // produces token descriptors from inconsistent or absent ERC20 symbol implementations that can return string or bytes32
 // this library will always produce a string symbol to represent the token
 library SafeERC20Namer {
-    function bytes32ToString(bytes32 x) private pure returns (string memory) {
-        bytes memory bytesString = new bytes(32);
-        uint256 charCount = 0;
-        for (uint256 j = 0; j < 32; j++) {
-            bytes1 char = x[j];
-            if (char != 0) {
-                bytesString[charCount] = char;
-                charCount++;
-            }
+
+    /**********************/
+    /*** View Functions ***/
+    /**********************/
+
+    // attempts to extract the token symbol. if it does not implement symbol, returns a symbol derived from the address
+    function tokenSymbol(address token) internal view returns (string memory) {
+        // 0x95d89b41 = bytes4(keccak256("symbol()"))
+        string memory symbol = callAndParseStringReturn(token, 0x95d89b41);
+        if (bytes(symbol).length == 0) {
+            // fallback to 6 uppercase hex of address string in upper case
+            return toAsciiString(token, 6);
         }
-        bytes memory bytesStringTrimmed = new bytes(charCount);
-        for (uint256 j = 0; j < charCount; j++) {
-            bytesStringTrimmed[j] = bytesString[j];
-        }
-        return string(bytesStringTrimmed);
+        return symbol;
     }
 
-    // assumes the data is in position 2
-    function parseStringData(bytes memory b) private pure returns (string memory) {
-        uint256 charCount = 0;
-        // first parse the charCount out of the data
-        for (uint256 i = 32; i < 64; i++) {
-            charCount <<= 8;
-            charCount += uint8(b[i]);
+    // attempts to extract the token name. if it does not implement name, returns a name derived from the address
+    function tokenName(address token) internal view returns (string memory) {
+        // 0x06fdde03 = bytes4(keccak256("name()"))
+        string memory name = callAndParseStringReturn(token, 0x06fdde03);
+        if (bytes(name).length == 0) {
+            // fallback to full hex of address string in upper case
+            return toAsciiString(token, 40);
         }
-
-        bytes memory bytesStringTrimmed = new bytes(charCount);
-        for (uint256 i = 0; i < charCount; i++) {
-            bytesStringTrimmed[i] = b[i + 64];
-        }
-
-        return string(bytesStringTrimmed);
+        return name;
     }
 
-    // uses a heuristic to produce a token name from the address
-    // the heuristic returns the full hex of the address string in upper case
-    function addressToName(address token) private pure returns (string memory) {
-        return AddressStringUtil.toAsciiString(token, 40);
-    }
-
-    // uses a heuristic to produce a token symbol from the address
-    // the heuristic returns the first 6 hex of the address string in upper case
-    function addressToSymbol(address token) private pure returns (string memory) {
-        return AddressStringUtil.toAsciiString(token, 6);
-    }
+    /*************************/
+    /*** Utility Functions ***/
+    /*************************/
 
     // calls an external view token contract method that returns a symbol or name, and parses the output into a string
     function callAndParseStringReturn(address token, bytes4 selector) private view returns (string memory) {
@@ -70,25 +53,54 @@ library SafeERC20Namer {
         return '';
     }
 
-    // attempts to extract the token symbol. if it does not implement symbol, returns a symbol derived from the address
-    function tokenSymbol(address token) internal view returns (string memory) {
-        // 0x95d89b41 = bytes4(keccak256("symbol()"))
-        string memory symbol = callAndParseStringReturn(token, 0x95d89b41);
-        if (bytes(symbol).length == 0) {
-            // fallback to 6 uppercase hex of address
-            return addressToSymbol(token);
+    /*********************************/
+    /*** Type Conversion Functions ***/
+    /*********************************/
+
+    function bytes32ToString(bytes32 x) private pure returns (string memory) {
+        bytes memory bytesString = new bytes(32);
+        uint256 charCount = 0;
+        for (uint256 j = 0; j < 32; j++) {
+            bytes1 char = x[j];
+            if (char != 0) {
+                bytesString[charCount] = char;
+                charCount++;
+            }
         }
-        return symbol;
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for (uint256 j = 0; j < charCount; j++) {
+            bytesStringTrimmed[j] = bytesString[j];
+        }
+        return string(bytesStringTrimmed);
     }
 
-    // attempts to extract the token name. if it does not implement name, returns a name derived from the address
-    function tokenName(address token) internal view returns (string memory) {
-        // 0x06fdde03 = bytes4(keccak256("name()"))
-        string memory name = callAndParseStringReturn(token, 0x06fdde03);
-        if (bytes(name).length == 0) {
-            // fallback to full hex of address
-            return addressToName(token);
+    // converts an address to the uppercase hex string, extracting only len bytes (up to 20, multiple of 2)
+    function toAsciiString(address addr, uint256 len) private pure returns (string memory) {
+        require(len % 2 == 0 && len > 0 && len <= 40, 'AddressStringUtil: INVALID_LEN');
+
+        bytes memory s = new bytes(len);
+        uint256 addrNum = uint256(uint160(addr));
+        for (uint256 i = 0; i < len / 2; i++) {
+            // shift right and truncate all but the least significant byte to extract the byte at position 19-i
+            uint8 b = uint8(addrNum >> (8 * (19 - i)));
+            // first hex character is the most significant 4 bits
+            uint8 hi = b >> 4;
+            // second hex character is the least significant 4 bits
+            uint8 lo = b - (hi << 4);
+            s[2 * i] = char(hi);
+            s[2 * i + 1] = char(lo);
         }
-        return name;
+        return string(s);
+    }
+
+    // hi and lo are only 4 bits and between 0 and 16
+    // this method converts those values to the unicode/ascii code point for the hex representation
+    // uses upper case for the characters
+    function char(uint8 b) private pure returns (bytes1 c) {
+        if (b < 10) {
+            return bytes1(b + 0x30);
+        } else {
+            return bytes1(b + 0x37);
+        }
     }
 }
