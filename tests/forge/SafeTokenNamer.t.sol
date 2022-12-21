1934a8d7bf241f65a44663592a0f833de8924634
Mike <mikehathaway@makerdao.com>
2022-12-21 16:24:49 -0500

    expand tests

--- tests/forge/SafeTokenNamer.t.sol
+++ tests/forge/SafeTokenNamer.t.sol
@@ -1,5 +1,3 @@
-
-
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.14;
 
@@ -7,9 +5,6 @@ import './utils/DSTestPlus.sol';
 import './utils/Tokens.sol';
 
 import 'src/libraries/SafeTokenNamer.sol';
-
-// https://github.com/Uniswap/solidity-lib/blob/master/test/SafeERC20Namer.spec.ts
-
 
 contract SafeTokenNamerTest is DSTestPlus {
 
@@ -24,17 +19,18 @@ contract SafeTokenNamerTest is DSTestPlus {
     NFTCollateralToken internal _nftCollateralOne;
 
     function setUp() external {
+        vm.createSelectFork(vm.envString("ETH_RPC_URL"));
+
         _ercCollateralOne = new Token("Collateral 1", "C1");
         _ercQuoteOne      = new Token("Quote 1", "Q1");
 
         _ercCollateralTwo = new Token("Collateral 2", "C2");
         _ercQuoteTwo      = new Token("Quote 2", "Q2");
 
-        _tokenLong      = new Token("TOKEN <TESTING LOTS OF CHARACTERS!!> 3", "TESTING_LONG_TOKEN_SYMBOL");
+        _tokenLong        = new Token("TOKEN <TESTING LOTS OF CHARACTERS!!> 3", "TESTING_LONG_TOKEN_SYMBOL");
 
         _nftCollateralOne = new NFTCollateralToken();
     }
-
 
     function testERC20Name() external {
         assertEq(SafeTokenNamer.tokenName(address(_ercCollateralOne)), "Collateral 1");
@@ -56,5 +52,15 @@ contract SafeTokenNamerTest is DSTestPlus {
 
     function testERC721Symbol() external {
         assertEq(SafeTokenNamer.tokenSymbol(address(_nftCollateralOne)), "NFTC");
+    }
+
+    function testMoonCatsMetadata() external {
+        assertEq(SafeTokenNamer.tokenName(0x7C40c393DC0f283F318791d746d894DdD3693572), "Wrapped MoonCatsRescue");
+        assertEq(SafeTokenNamer.tokenSymbol(0x7C40c393DC0f283F318791d746d894DdD3693572), "WMCR");
+    }
+
+    function testWETHMetadata() external {
+        assertEq(SafeTokenNamer.tokenName(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2), "Wrapped Ether");
+        assertEq(SafeTokenNamer.tokenSymbol(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2), "WETH");
     }
 }
