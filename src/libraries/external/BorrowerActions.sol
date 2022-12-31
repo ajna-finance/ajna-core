ba916ec1a05a029e1bf508e1ae8dfd6347619ba0
grandizzy <grandizzy.the.egg@gmail.com>
2022-12-31 19:15:22 +0200

    Take 2

---
+++ src/libraries/external/BorrowerActions.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity 0.8.14;
+
+/**
+    @notice External library containing logic for common borrower actions.
+ */
+library BorrowerActions {
+
+}
