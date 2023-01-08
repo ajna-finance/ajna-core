987805d7a58c014985367cf1a3380951744abc1d
grandizzy <grandizzy.the.egg@gmail.com>
2023-01-08 02:07:06 +0200

    Fix deploy script, remove field separator

--- deploy.sh
+++ deploy.sh
@@ -32,20 +32,18 @@ do
 done
 
 echo Deploying factories...
-factories=( 'ERC20PoolFactory' 'ERC721PoolFactory' )
+factories=( ERC20PoolFactory ERC721PoolFactory )
 for contract in "${factories[@]}"
 do
-    IFS=',' read -r -a arr <<< ${contract}
-    name=${arr[1]}
     createfactory="forge create --rpc-url ${ETH_RPC_URL:?} --keystore ${DEPLOY_KEY:?} --password ${password:?} \
-        src/$name.sol:$name --constructor-args ${AJNA_TOKEN:?} ${linkage}"
+        src/$contract.sol:$contract --constructor-args ${AJNA_TOKEN:?} ${linkage}"
     output=$($createfactory)
     if [[ $output =~ $regex ]]
     then
         address=${BASH_REMATCH[1]}
-        printf "Deployed %20s to %s\n" ${name:0:20} $address
+        printf "Deployed %20s to %s\n" ${contract:0:20} $address
     else
-        echo $name was not deployed: $output
+        echo $contract was not deployed: $output
         exit 2
     fi
 done
