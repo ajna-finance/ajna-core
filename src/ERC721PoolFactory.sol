1c6d1934f6915c8c6b8466c180cb1b574e611393
Mike Hathaway <mahathaway93@gmail.com>
2023-02-12 18:38:18 -0500

    Sherlock 73 (#591)
    * Create standardized PR templates for contracts (#539)
    
    This PR creates three github PR templates that should be followed:
    * `logic_src_changes.md`
    * `cosmetic_src_changes.md`
    * `testing_or_misc_nonsrc_changes.md`
    
    To learn more about how GH templates work -> https://docs.github.com/en/communities/using-templates-to-encourage-useful-issues-and-pull-requests/creating-a-pull-request-template-for-your-repository
    
    Co-authored-by: Ian Harvey <iharvey@comcast.net>
    
    * Single template (#546)
    
    * merged all of the templates into one so the text will auto fill when a PR is created.
    * was running into this issue documented here with auto fill -> https://stackoverflow.com/questions/52139192/github-pull-requests-template-not-showing
    
    * update getNFTSubsetHash to uniquely sort array of tokenIds
    
    * add natspec and slight gas optimization
    
    * change encodePacked to encode to add byte padding
    
    * pool deployer gas optimization
    
    * check sort order instead of sorting on chain
    
    * pr feedback to check for duplicate tokenIds
    
    ---------
    
    Co-authored-by: Ian Harvey <ith.harvey@gmail.com>
    Co-authored-by: Ian Harvey <iharvey@comcast.net>
    Co-authored-by: Mike <mikehathaway@makerdao.com>

--- src/ERC721PoolFactory.sol
+++ src/ERC721PoolFactory.sol
@@ -5,8 +5,8 @@ pragma solidity 0.8.14;
 import { ClonesWithImmutableArgs } from '@clones/ClonesWithImmutableArgs.sol';
 import { IERC165 }                 from '@openzeppelin/contracts/utils/introspection/IERC165.sol';
 
-import { IERC721PoolFactory } from './interfaces/pool/erc721/IERC721PoolFactory.sol';
-
+import { IERC721PoolFactory }    from './interfaces/pool/erc721/IERC721PoolFactory.sol';
+import { IPoolFactory }          from './interfaces/pool/IPoolFactory.sol';
 import { IERC20Token, PoolType } from './interfaces/pool/IPool.sol';
 
 import { ERC721Pool }   from './ERC721Pool.sol';
@@ -52,7 +52,10 @@ contract ERC721PoolFactory is PoolDeployer, IERC721PoolFactory {
      */
     function deployPool(
         address collateral_, address quote_, uint256[] memory tokenIds_, uint256 interestRate_
-    ) external canDeploy(getNFTSubsetHash(tokenIds_), collateral_, quote_, interestRate_) returns (address pool_) {
+    ) external canDeploy(collateral_, quote_, interestRate_) returns (address pool_) {
+        bytes32 subsetHash = getNFTSubsetHash(tokenIds_);
+        if (deployedPools[subsetHash][collateral_][quote_] != address(0)) revert IPoolFactory.PoolAlreadyExists();
+
         uint256 quoteTokenScale = 10**(18 - IERC20Token(quote_).decimals());
 
         try IERC165(collateral_).supportsInterface(0x80ac58cd) returns (bool supportsERC721Interface) {
@@ -75,7 +78,7 @@ contract ERC721PoolFactory is PoolDeployer, IERC721PoolFactory {
         pool_ = address(pool);
 
         // Track the newly deployed pool
-        deployedPools[getNFTSubsetHash(tokenIds_)][collateral_][quote_] = pool_;
+        deployedPools[subsetHash][collateral_][quote_] = pool_;
         deployedPoolsList.push(pool_);
 
         emit PoolCreated(pool_);
@@ -87,8 +90,36 @@ contract ERC721PoolFactory is PoolDeployer, IERC721PoolFactory {
     /*** Pool Creation Functions ***/
     /*******************************/
 
+    /**
+     *  @notice Get the hash of the subset of NFTs that will be used to create the pool
+     *  @dev    If no tokenIds are provided, the default ERC721_NON_SUBSET_HASH is returned
+     *  @param  tokenIds_ The array of token ids that will be used to create the pool
+     *  @return bytes32 The hash of the subset of NFTs that will be used to create the pool
+     */
     function getNFTSubsetHash(uint256[] memory tokenIds_) public pure returns (bytes32) {
         if (tokenIds_.length == 0) return ERC721_NON_SUBSET_HASH;
-        else return keccak256(abi.encodePacked(tokenIds_));
+        else {
+            // check the array of token ids is sorted in ascending order
+            // revert if not sorted
+            _checkTokenIdSortOrder(tokenIds_);
+
+            // hash the sorted array of tokenIds
+            return keccak256(abi.encode(tokenIds_));
+        }
     }
+
+    /**
+     *  @notice Check that the array of token ids is sorted in ascending order, else revert.
+     *  @dev    The counters are modified in unchecked blocks due to being bounded by array length
+     *  @param  tokenIds_ The array of token ids to check for sorting
+     */
+    function _checkTokenIdSortOrder(uint256[] memory tokenIds_) internal pure {
+        for (uint256 i = 0; i < tokenIds_.length - 1; ) {
+            if (tokenIds_[i] >= tokenIds_[i + 1]) revert TokenIdSubsetInvalid();
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
 }
