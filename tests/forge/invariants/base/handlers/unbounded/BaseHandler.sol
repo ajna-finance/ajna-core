365f368dabd3ab2d98ae2487f58436a39d4e5a07
grandizzy <grandizzy.the.egg@gmail.com>
2023-05-04 17:42:34 +0300

    Invariants improvement: Make time to kick configurable SKIP_TIME_TO_KICK

--- tests/forge/invariants/base/handlers/unbounded/BaseHandler.sol
+++ tests/forge/invariants/base/handlers/unbounded/BaseHandler.sol
@@ -171,6 +171,10 @@ abstract contract BaseHandler is Test {
         _pool.updateInterest();
     }
 
+    function _getKickSkipTime() internal returns (uint256) {
+        return vm.envOr("SKIP_TIME_TO_KICK", uint256(24 hours));
+    }
+
     /**
      * @dev Ensure that error is an Pool expected error.
      */
