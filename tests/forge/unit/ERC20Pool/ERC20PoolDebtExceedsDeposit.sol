91a90ce8b7f926ee5880ee7307725fe54a38daf6
Ian Harvey <iharvey@comcast.net>
2023-11-03 07:19:05 -0400

    added Matts test WIP

--- tests/forge/unit/ERC20Pool/ERC20PoolDebtExceedsDeposit.sol
+++ tests/forge/unit/ERC20Pool/ERC20PoolDebtExceedsDeposit.sol
@@ -9,12 +9,14 @@ import '@std/console.sol';
 
 import 'src/ERC20Pool.sol';
 
+import '@std/console.sol';
+
 contract ERC20PoolBorrowTest is ERC20HelperContract {
 
     address internal _borrower;
     address internal _borrower2;
     address internal _lender;
-    address internal _lender1;
+    address internal _attacker;
 
     function setUp() external {
         _startTest();
@@ -22,11 +24,14 @@ contract ERC20PoolBorrowTest is ERC20HelperContract {
         _borrower  = makeAddr("borrower");
         _borrower2 = makeAddr("borrower2");
         _lender    = makeAddr("lender");
+        _attacker  = makeAddr("attacker");
 
         _mintCollateralAndApproveTokens(_borrower,  1_000 * 1e18);
         _mintCollateralAndApproveTokens(_borrower2,  1_000 * 1e18);
+        _mintCollateralAndApproveTokens(_attacker,  10 * 1e18);
 
         _mintQuoteAndApproveTokens(_lender,   200_000 * 1e18);
+        _mintQuoteAndApproveTokens(_attacker, 200_000 * 1e18);
 
 
         // lender deposits 1_000 quote in price of 1.0
@@ -95,6 +100,16 @@ contract ERC20PoolBorrowTest is ERC20HelperContract {
             settledDebt: 989.604567844708659388 * 1e18
         });
 
+        vm.expectEmit(true, true, false, true);
+        emit BucketBankruptcy(7388, 0);
+
+        _settle({
+            from:        _lender,
+            borrower:    _borrower,
+            maxDepth:    10,
+            settledDebt: 0
+        });
+
         _assertAuction(
             AuctionParams({
                 borrower:          _borrower,
@@ -125,4 +140,135 @@ contract ERC20PoolBorrowTest is ERC20HelperContract {
         vm.expectRevert(abi.encodeWithSignature('AuctionNotCleared()'));
         _pool.addQuoteToken(1_000 * 1e18, 4160, type(uint256).max, false);
     }
+
+
+    function testStealReservesWithMargin() external {
+
+        // Deposit 100 qt at a price of 1
+        _removeLiquidity({
+            from:     _lender,
+            amount:   900.0 * 1e18,
+            index:    4156,
+            newLup:   1004968987.606512354182109771 * 1e18,
+            lpRedeem: 900.0 * 1e18
+        });
+
+        // 1b. Legit borrower posts 75 collateral and borrows 50 quote token
+        // first borrower adds collateral token and borrows
+        _pledgeCollateral({
+            from:     _borrower,
+            borrower: _borrower,
+            amount:   75.0 * 1e18
+        });
+
+        _borrow({
+            from:       _borrower,
+            amount:     50.0 * 1e18,
+            indexLimit: 7388,
+            newLup:     1.0 * 1e18
+        });
+
+        _assertPool(
+            PoolParams({
+                htp:                  667307692307692308,
+                lup:                  1 * 1e18,
+                poolSize:             100.0 * 1e18,
+                pledgedCollateral:    75.000000000000000000 * 1e18,
+                encumberedCollateral: 50048076923076923100,
+                poolDebt:             50.048076923076923100 * 1e18,
+                actualUtilization:    0,
+                targetUtilization:    1.0 * 1e18,
+                minDebtAmount:        5004807692307692310,
+                loans:                1,
+                maxBorrower:          address(_borrower),
+                interestRate:         0.05 * 1e18,
+                interestRateUpdate:   block.timestamp
+            })
+        );
+
+        // 50 quote token to the pool address, just to create a bunch of reserves.
+        deal(address(_quote), address(_pool), 50.0 * 1e18);
+
+        _assertPool(
+            PoolParams({
+                htp:                  667307692307692308,
+                lup:                  1 * 1e18,
+                poolSize:             100.0 * 1e18,
+                pledgedCollateral:    75.000000000000000000 * 1e18,
+                encumberedCollateral: 50048076923076923100,
+                poolDebt:             50.048076923076923100 * 1e18,
+                actualUtilization:    0,
+                targetUtilization:    1.0 * 1e18,
+                minDebtAmount:        5004807692307692310,
+                loans:                1,
+                maxBorrower:          address(_borrower),
+                interestRate:         0.05 * 1e18,
+                interestRateUpdate:   block.timestamp
+            })
+        );
+
+        // Attacker does the following in quick succession (ideally same block):
+        console.log(_indexOf(100.0 * 1e18), "price");
+
+        // 2a. deposits 200 quote token at price 100
+
+        _addLiquidity({
+            from:    _attacker,
+            amount:  200.0 * 1e18,
+            index:   3232,
+            lpAward: 200 * 1e18,
+            newLup:  100.332368143282009890 * 1e18
+        });
+
+        // 2b. posts 1.04 collateral and borrows 100 quote token
+        _pledgeCollateral({
+            from:     _attacker,
+            borrower: _attacker,
+            amount:   1.04 * 1e18
+        });
+
+        _borrow({
+            from:       _attacker,
+            amount:     100.0 * 1e18,
+            indexLimit: 7388,
+            newLup:     100332368143282009890
+        });
+
+        // 2c. lenderKicks the loan in 2b using deposit in 2a
+        _lenderKick({
+            from:       _attacker,
+            index:      3232,
+            borrower:   _attacker,
+            debt:       100.096153846153846200 * 1e18,
+            collateral: 1.040000000000000000 * 1e18,
+            bond:       1.119109021431385084 * 1e18
+        });
+
+        // 2d. withdraws 100 of the deposit from 2a
+        _removeLiquidity({
+            from:     _lender,
+            amount:   10.0 * 1e18,
+            index:    3232,
+            newLup:   1004968987.606512354182109771 * 1e18,
+            lpRedeem: 100.0 * 1e18
+        });
+
+        // Now wait until auction price drops to about $50
+
+        // In a single block finish the attack:
+        // 2a. Call arbtake using 100 price bucket
+        // 2b. Call settle
+        // 2c. Withdraw the deposit remaing (should be about 50) and the collateral moved (should be 1.04) from the 100 price bucket (all go to the attacker)
+
+
+
+
+    }
+
+
+
+
+
+
+
 }
