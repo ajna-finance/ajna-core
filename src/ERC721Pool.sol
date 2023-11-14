ea03a07a8a83accd3aa523f9e56e54bf192c09a3
Prateek Gupta <prateek105@users.noreply.github.com>
2023-11-14 20:09:12 +0530

    Add debt and collateral settled returns in settle function (#960)
    * Return debt and collateral settled from settle method
    
    * Add isBorrowerSettled return in settle method

--- src/ERC721Pool.sol
+++ src/ERC721Pool.sol
@@ -394,7 +394,7 @@ contract ERC721Pool is FlashloanablePool, IERC721Pool {
     function settle(
         address borrowerAddress_,
         uint256 maxDepth_
-    ) external nonReentrant override {
+    ) external nonReentrant override returns (uint256 collateralSettled_, bool isBorrowerSettled_) {
         PoolState memory poolState = _accruePoolInterest();
 
         SettleParams memory params = SettleParams({
@@ -417,6 +417,9 @@ contract ERC721Pool is FlashloanablePool, IERC721Pool {
 
         // move token ids from borrower array to pool claimable array if any collateral used to settle bad debt
         _rebalanceTokens(params.borrower, result.collateralRemaining);
+
+        collateralSettled_ = result.collateralSettled;
+        isBorrowerSettled_ = (result.debtPostAction == 0);
     }
 
     /**
