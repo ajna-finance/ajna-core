a28c04bea939bdfafc58acdc3345c695f17a90a0
Ian Harvey <iharvey@comcast.net>
2023-12-07 14:03:40 -0500

    updated

--- tests/forge/unit/ERC721Pool/ERC721PoolLiquidationsHighTake.t.sol
+++ tests/forge/unit/ERC721Pool/ERC721PoolLiquidationsHighTake.t.sol
@@ -24,24 +24,9 @@ contract ERC721PoolLiquidationsTakeTest is ERC721HelperContract {
         _withdrawRecipient = makeAddr("withdrawRecipient");
 
         // deploy subset pool
-        uint256[] memory subsetTokenIds = new uint256[](1);
+        uint256[] memory subsetTokenIds = new uint256[](2);
         subsetTokenIds[0] = 1;
-        // subsetTokenIds[1] = 3;
-        // subsetTokenIds[2] = 5;
-        // subsetTokenIds[3] = 6;
-        // subsetTokenIds[4] = 7;
-        // subsetTokenIds[5] = 8;
-        // subsetTokenIds[6] = 9;
-        // subsetTokenIds[7] = 10;
-        // subsetTokenIds[8] = 11;
-        // subsetTokenIds[9] = 12;
-        // subsetTokenIds[10] = 13;
-        // subsetTokenIds[11] = 14;
-        // subsetTokenIds[12] = 15;
-        // subsetTokenIds[13] = 51;
-        // subsetTokenIds[14] = 53;
-        // subsetTokenIds[15] = 73;
-        // subsetTokenIds[16] = 76;
+        subsetTokenIds[1] = 2;
 
         _pool = _deploySubsetPool(subsetTokenIds);
 
@@ -75,78 +60,9 @@ contract ERC721PoolLiquidationsTakeTest is ERC721HelperContract {
             newLup:     999_969_141.897027226245329498 * 1e18
         });
 
-        // second borrower deposits three NFTs into the subset pool and borrows
-        // tokenIdsToAdd = new uint256[](12);
-        // tokenIdsToAdd[0] = 7;
-        // tokenIdsToAdd[1] = 8;
-        // tokenIdsToAdd[2] = 9;
-        // tokenIdsToAdd[3] = 10;
-        // tokenIdsToAdd[4] = 11;
-        // tokenIdsToAdd[5] = 12;
-        // tokenIdsToAdd[6] = 13;
-        // tokenIdsToAdd[7] = 14;
-        // tokenIdsToAdd[8] = 15;
-        // tokenIdsToAdd[9] = 51;
-        // tokenIdsToAdd[10] = 53;
-        // tokenIdsToAdd[11] = 73;
-
-        // _pledgeCollateral({
-        //     from:     _borrower2,
-        //     borrower: _borrower2,
-        //     tokenIds: tokenIdsToAdd
-        // });
-        // _borrow({
-        //     from:       _borrower2,
-        //     amount:     1.0 * 1e18,
-        //     indexLimit: _i9_72,
-        //     newLup:     946_585_655.625225796879504875 * 1e18
-        // });
-
-        // tokenIdsToAdd = new uint256[](1);
-        // tokenIdsToAdd[0] = 76;
-
-        // _pledgeCollateral({
-        //     from:     _borrower3,
-        //     borrower: _borrower3,
-        //     tokenIds: tokenIdsToAdd
-        // });
-        // _borrow({
-        //     from:       _borrower3,
-        //     amount:     2_900.0 * 1e18,
-        //     indexLimit: _i9_72,
-        //     newLup:     946_585_655.625225796879504875 * 1e18
-        // });
 
         skip(100 days);
 
-        /*****************************/
-        /*** Assert pre-kick state ***/
-        /*****************************/
-
-        // _assertPool(
-        //     PoolParams({
-        //         htp:                  13012.500000000000006000 * 1e18,
-        //         lup:                  956075176.822868670011708730 * 1e18,
-        //         poolSize:             72_996.666666666666667000 * 1e18,
-        //         pledgedCollateral:    5 * 1e18,
-        //         encumberedCollateral: 0.000027236995197946 * 1e18,
-        //         poolDebt:             25039.052884615384626930 * 1e18,
-        //         actualUtilization:    0,
-        //         targetUtilization:    1 * 1e18,
-        //         minDebtAmount:        1251.952644230769231347 * 1e18,
-        //         loans:                2,
-        //         maxBorrower:          address(_borrower),
-        //         interestRate:         0.05 * 1e18,
-        //         interestRateUpdate:   _startTime
-        //     })
-        // );
-        // _assertBorrower({
-        //     borrower:                  _borrower,
-        //     borrowerDebt:              25024.038461538461550000 * 1e18,
-        //     borrowerCollateral:        2 * 1e18,
-        //     borrowert0Np:              13910.905507558462180020 * 1e18,
-        //     borrowerCollateralization: 73473.596681872712358952 * 1e18
-        // });
         _assertBorrower({
             borrower:                  _borrower,
             borrowerDebt:              974735101.867470788027259956 * 1e18,
