be8718b644f230aefa7e31c6ab469877a01a7584
grandizzy <38490174+grandizzy@users.noreply.github.com>
2023-12-16 18:31:16 +0200

    `_revertIfActiveAuctions` helper should revert with `ActiveAuction` error (#1031)
    * _revertIfActiveAuctions helper should revert with ActiveAuction error
    
    * Fix comment

--- src/base/Pool.sol
+++ src/base/Pool.sol
@@ -395,7 +395,7 @@ abstract contract Pool is Clone, ReentrancyGuard, Multicall, IPool {
      *  @dev    update `reserveAuction.latestBurnEventEpoch` and burn event `timestamp` state
      *  @dev    === Reverts on ===
      *  @dev    2 weeks not passed `ReserveAuctionTooSoon()`
-     *  @dev    unsettled liquidation `AuctionNotCleared()`
+     *  @dev    unsettled liquidation `AuctionActive()`
      *  @dev    === Emit events ===
      *  @dev    - `KickReserveAuction`
      */
